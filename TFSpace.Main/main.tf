d4c0da0379e0bc1446c859bfd5f05ebe220d7e20
nthskyradiated <andy.pandaan@outlook.com>
2022-03-14 21:44:24 +0400

    3rd Commit
    Added .gitignore

--- TFSpace.Main/main.tf
+++ TFSpace.Main/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "TF-Ubuntu1" {
-  ami           = "04505e74c074"
-  instance_type = "t3.micro"
+  ami           = "ami-04505e74c0741db8d"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "TF-Ubuntu1"
