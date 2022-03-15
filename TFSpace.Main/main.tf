6dd46e087358440d446ce5882c552e203490f8a0
nthskyradiated <71885584+nthskyradiated@users.noreply.github.com>
2022-03-15 13:36:43 +0400

    added VPC
    Added VPC Resource

--- TFSpace.Main/main.tf
+++ TFSpace.Main/main.tf
@@ -5,3 +5,31 @@ resource "aws_instance" "TF-Ubuntu1" {
     Name = "TF-Ubuntu1"
   }
 }
+ resource "aws_vpc" "Production" {
+    cidr_block = "10.0.0.0/16"
+     tags = {
+      Name = "Production VPC"
+     }
+  }
+   resource "aws_subnet" "mainsub" {
+   vpc_id     = aws_vpc.Production.id
+   cidr_block = "10.0.1.0/24"
+   availability_zone = "us-east-1a"
+
+
+   tags = {
+     Name = "Main-Sub"
+   }
+ }
+
+  resource "aws_subnet" "devsub" {
+   vpc_id     = aws_vpc.Production.id
+   cidr_block = "10.0.2.0/24"
+   availability_zone = "us-east-1a"
+
+
+   tags = {
+     Name = "Dev-Sub"
+
+   }
+ }
